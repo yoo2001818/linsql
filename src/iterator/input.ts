a03209d23c36b8db301f756f12ba254536702cfb
Duknam Yoo <yoo2001818@gmail.com>
2018-07-04 23:58:14 +0900

    Implement sync input iterator

--- src/iterator/input.ts
+++ src/iterator/input.ts
@@ -1,7 +1,20 @@
 import { Row } from '../row';
 
-export default async function * inputIterator(
-  input: Row[],
-): AsyncIterator<Row[]> {
-  yield input;
+export default class InputIterator implements Iterator<Row> {
+  input: Row[];
+  position: number;
+  constructor(input: Row[]) {
+    this.input = input;
+    this.position = 0;
+  }
+  next(): IteratorResult<Row> {
+    let value = this.input[this.position ++];
+    return {
+      value,
+      done: this.position >= this.input.length,
+    }
+  }
+  [Symbol.iterator]() {
+    return this;
+  }
 }
