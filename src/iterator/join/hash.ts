aed3da56ab09de0b38b9c7d143c63cbaf45a538b
Duknam Yoo <yoo2001818@gmail.com>
2018-07-18 23:57:29 +0900

    Rewrite hash join comments

--- src/iterator/join/hash.ts
+++ src/iterator/join/hash.ts
@@ -18,12 +18,21 @@ export default class HashJoinIterator implements RowIterator {
     // Only =, in, AND, OR is allowed in here.
     // We separate each row into 'bucket', 'tuple', 'corresponding value'
     // [a, b] -> [c, d] means (a, b) should match against (c, d)
-    // Case 1) a = b: [a] -> [b] 
-    // Case 2) a = b OR c = d: [a] -> [b] OR [c] -> [d]
-    // Case 3) a = b AND c = d: [a, c] -> [b, d]
-    // Case 4) a = b AND c = b: [a] -> [b], additional c = b check
-    // Case 5) a = b AND a = d: [a] -> [b] AND [a] -> [d]
-    // Case 6) a = b OR a = d: [a] -> [b] OR [a] -> [d]
-    // Case 7) a = b OR a = d: [a] -> [b | d]
+    // Case 1) a.1 = b.1: [a.1] -> [b.1]
+    // Case 2) a.1 = b.1 AND a.2 = b.2: [a.1, a.2] -> [b.1, b.2]
+    // Case 3) a.1 = b.1 AND a.2 = b.1: [a.1] -> [b.1] AND a.1 = a.2 prefilter
+    // Case 4) a.1 = b.1 AND a.1 = b.2: [a.1] -> [b.1] AND b.1 = b.2 prefilter
+    // Case 5) a.1 = b.1 OR a.2 = b.2: [a.1] -> [b.1] OR [a.2] -> [b.2]
+    // Case 6) a.1 = b.1 OR a.2 = b.1: [a.1 | a.2] -> [b.1]
+    // Case 7) a.1 = b.1 OR a.1 = b.2: [a.1] -> [b.1 | b.2]
+    // Case 8) (...) AND a.3 = b.3: [a.3] -> [b.3] AND after filter
+    // Case 9) (a.1 = b.1 AND a.2 = b.2) OR a.3 = b.3:
+    //   [a.1, a.2] -> [b.1, b.2] OR [a.3] -> [b.3]
+    // Basically, we can perform multiple hash joins lookups for OR, but
+    // for AND this is a really bad idea.
+    // Extract hashtables and clauses like this:
+    // $0: [b.1], $0: [b.2]
+    // Test a.1 = a.2
+    // Fetch a.1 -> $0
   }
 }
