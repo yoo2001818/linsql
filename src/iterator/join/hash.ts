f5396df2bba8f2daf9b68eb88dc172d6ba88ae40
Duknam Yoo <yoo2001818@gmail.com>
2018-08-05 23:57:56 +0900

    Implement hash join left / right hashing

--- src/iterator/join/hash.ts
+++ src/iterator/join/hash.ts
@@ -1,4 +1,5 @@
 import { Expression } from 'yasqlp';
+import objectHash from 'object-hash';
 import { Row } from '../../row';
 import planHashJoin, { HashJoinPlan } from '../../hashJoinPlanner';
 import RowIterator from '../type';
@@ -9,7 +10,7 @@ export default class HashJoinIterator implements RowIterator {
   left: RowIterator;
   right: RowIterator;
   leftJoin: boolean;
-  tables: { [key: string]: any }[];
+  tables: { [key: string]: Row[] }[];
   plan: HashJoinPlan;
   tablePlans: { tableId: number, evaluate: (row: Row) => any }[];
   comparePlans: { tableId: number, evaluate: (row: Row) => any }[];
@@ -45,12 +46,44 @@ export default class HashJoinIterator implements RowIterator {
     if (this.tables == null) {
       this.tables = this.plan.tables.map(() => ({}));
       // Fetch and put each row to hash table
-      let it = this.left;
+      let it = this.right;
       while (true) {
         let result = await it.next(arg);
         if (result.done) break;
+        let rows = result.value;
+        for (let i = 0; i < rows.length; ++i) {
+          let row = rows[i];
+          this.tablePlans.forEach((plan) => {
+            let hash = objectHash(plan.evaluate(row));
+            let table = this.tables[plan.tableId];
+            if (table[hash] == null) {
+              table[hash] = [row];
+            } else {
+              table[hash].push(row);
+            }
+          });
+        }
       }
     }
+    // After generating hash table, compare against them.
+    let { done, value } = await this.left.next();
+    if (done) return { done, value };
+    let output: Row[] = [];
+    for (let i = 0; i < value.length; ++i) {
+      let row = value[i];
+      // TODO Dedupe exactly same output
+      this.comparePlans.forEach((plan) => {
+        let hash = objectHash(plan.evaluate(row));
+        let table = this.tables[plan.tableId];
+        if (table[hash] != null) {
+          table[hash].forEach((otherRow) => {
+            let newRow = { ...row, ...otherRow };
+            output.push(newRow);
+          });
+        }
+      });
+    }
+    return { done, value: output };
   }
   getTables() {
     return [...this.left.getTables(), ...this.right.getTables()];
