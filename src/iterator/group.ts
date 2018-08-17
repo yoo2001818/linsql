11a75f0b4bfc8c489a4919257a925dc5c9df3c70
Duknam Yoo <yoo2001818@gmail.com>
2018-08-17 23:56:56 +0900

    Implement group by comparing

--- src/iterator/group.ts
+++ src/iterator/group.ts
@@ -1,12 +1,22 @@
 import { Expression } from 'yasqlp';
+import deepEqual from 'deep-equal';
 import { Row } from '../row';
 import RowIterator from './type';
-import compileExpression from '../util/compileExpression';
+import compileExpression, { getCode } from '../util/compileExpression';
+
+type AggregateData = {
+  min?: any,
+  max?: any,
+  count?: any,
+  sum?: any,
+  sumSquared?: any,
+};
 
 export default class GroupIterator implements RowIterator {
   input: RowIterator;
   groupTargets: ((input: Row, parentRow: Row) => any)[];
   aggregates: Expression[];
+  aggregateData: AggregateData[];
   lastValue: any[];
   parentRow: Row;
   constructor(
@@ -15,12 +25,34 @@ export default class GroupIterator implements RowIterator {
     this.input = input;
     this.groupTargets = group.map(v => compileExpression(input.getTables(), v));
     this.aggregates = aggregates;
-    // TODO Ensure getOrder matches with group targets
+    // TODO Compare cardinality, ignore entry if cardinality is 1
+    let originalOrder = input.getOrder();
+    for (let i = 0; i < group.length; ++i) {
+      if (!deepEqual(originalOrder[i].value, group[i])) {
+        throw new Error('Order must match with group clause. Failed at: ' +
+          getCode({ tables: input.getTables() }, group[i]));
+      }
+    }
   }
   async next(arg?: any): Promise<IteratorResult<Row[]>> {
     let { value, done } = await this.input.next(arg);
-    if (done) return { value, done: true };
-    // TODO
+    let output: Row[] = [];
+    if (done) {
+      // TODO Output if aggregation data is not null
+      return { value, done: true };
+    }
+    for (let i = 0; i < value.length; ++i) {
+      let row = value[i];
+      let groupValue = this.groupTargets.map((evaluate) =>
+        evaluate(this.parentRow, row));
+      let matched = this.lastValue != null &&
+        groupValue.every((v, i) => v === this.lastValue[i]);
+      if (!matched) {
+        // TODO Output if aggregation data is not null
+        this.lastValue = groupValue;
+        // TODO Initialize aggregation data
+      }
+    }
     return {
       value,
       done: false,
@@ -37,6 +69,7 @@ export default class GroupIterator implements RowIterator {
   }
   rewind(parentRow: Row) {
     this.parentRow = parentRow;
+    this.lastValue = null;
     return this.input.rewind(parentRow);
   }
   [Symbol.asyncIterator]() {
