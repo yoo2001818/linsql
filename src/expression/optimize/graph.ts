c5be899446347de5c8156e81053d28950ca6a680
Duknam Yoo <yoo2001818@gmail.com>
2018-09-13 23:55:12 +0900

    Implement graph connecting with each other

--- src/expression/optimize/graph.ts
+++ src/expression/optimize/graph.ts
@@ -4,6 +4,7 @@ import deepEqual from 'deep-equal';
 import { rewrite } from '../traverse';
 
 type AndGraphNode = {
+  id: number,
   names: Expression[],
   connections: {
     op: CompareExpression['op'],
@@ -27,31 +28,55 @@ function isConstant(expr: Expression) {
   return ['number', 'boolean', 'string'].includes(expr.type);
 }
 
+function findNode(expr: Expression, nodes: AndGraphNode[]) {
+  let currentNode = nodes.find(node =>
+    node.names.some(v => deepEqual(v, expr)));
+  if (currentNode == null) {
+    currentNode = {
+      id: nodes.length,
+      names: [expr],
+      connections: [],
+      constants: [],
+    };
+    nodes.push(currentNode);
+  }
+  return currentNode;
+}
+
+function handleCompare(
+  op: CompareExpression['op'], left: Expression, right: Expression,
+  nodes: AndGraphNode[],
+) {
+  let currentNode = findNode(left, nodes);
+  if (!isConstant(right)) {
+    // Find a node associated with right node and add connection to it.
+    let rightNode = findNode(right, nodes);
+    currentNode.connections.push({ op, id: rightNode.id });
+    rightNode.connections.push({
+      op: COMPARE_REVERSES[op],
+      id: currentNode.id,
+    });
+  } else {
+    currentNode.constants.push({
+      op,
+      value: right,
+    });
+  }
+}
+
+const COMPARE_REVERSES = {
+  '=': '=' as '=',
+  '!=': '!=' as '!=',
+  '>=': '<=' as '<=',
+  '<=': '>=' as '>=',
+  '>': '<' as '<',
+  '<': '>' as '>',
+  'is': 'is' as 'is',
+  'like': 'like' as 'like',
+};
+
 export default function generateGraph(input: Expression) {
   // Recursively descend into AND nodes.
-  function handleCompare(
-    op: CompareExpression['op'], left: Expression, right: Expression,
-    nodes: AndGraphNode[],
-  ) {
-    let currentNode = nodes.find(node =>
-      node.names.some(v => deepEqual(v, left)));
-    if (currentNode == null) {
-      currentNode = {
-        names: [left],
-        connections: [],
-        constants: [],
-      };
-      nodes.push(currentNode);
-    }
-    if (!isConstant(right)) {
-      
-    } else {
-      currentNode.constants.push({
-        op,
-        value: right,
-      });
-    }
-  }
   return rewrite(input, {}, (expr, state) => {
     if (expr.type === 'logical' && expr.op === '&&') {
       let nodes: AndGraphNode[] = [];
@@ -60,10 +85,11 @@ export default function generateGraph(input: Expression) {
         if (value.type === 'compare') {
           // Connection is performed on both side, left and right.
           if (!isConstant(value.left)) {
-            handleCompare(value.left, value.right, nodes);
+            handleCompare(value.op, value.left, value.right, nodes);
           }
           if (!isConstant(value.right)) {
-            handleCompare(value.right, value.left, nodes);
+            handleCompare(COMPARE_REVERSES[value.op],
+              value.right, value.left, nodes);
           }
         } else if (value.type === 'boolean') {
           if (value.value === false) {
