bb32632bfa2cf7506cd8457a7cd8ed6c982359c4
Duknam Yoo <yoo2001818@gmail.com>
2018-09-26 00:03:35 +0900

    Upgrade dependencies to latest version

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,16 @@
 module.exports = {
   globals: {
     'ts-jest': {
-      tsConfigFile: 'tsconfig.json',
+      tsConfig: 'tsconfig.json',
     },
   },
   moduleFileExtensions: [
     'ts',
     'js',
   ],
-  transform: {
-    '^.+\\.(ts|tsx)$': './node_modules/ts-jest/preprocessor.js',
-  },
   testMatch: [
     '**/test/**/*.test.(ts|js)',
   ],
   testEnvironment: 'node',
+  preset: 'ts-jest',
 };
