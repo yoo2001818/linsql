333ce86fdad15457cf09a2d91f303511127a66bd
Duknam Yoo <yoo2001818@gmail.com>
2018-09-27 23:59:15 +0900

    Add comment about algebra optimization

--- src/expression/optimize/algebra.ts
+++ src/expression/optimize/algebra.ts
@@ -38,9 +38,43 @@ export function rewriteCompareColumn(expr: Expression) {
  * As you can see, this is completely non-trivial and requires some effort.
  * @param expr The expression to convert to SARGs if possible.
  */
+
+ // a + b = 0 -> a = -b
+ // a + 3 * 5 -> a + 15
+ // a / 2 + 3 = 0 -> a / 2 = -3 -> a = -6
+ 
+ // 1. expansion of trapped columns
+ // 2. push all constants to right
+ // 3. evaluate all constants
+ // 4. push other column to the right
 export function rewriteSargable(expr: Expression) {
   return rewrite(expr, {}, (expr, state) => {
     if (expr.type === 'compare') {
+      /**
+       * We need to perform a lot of operations to rescue those poor SARGable
+       * expressions - we need to exploit bunch of algebra properties to
+       * make them right.
+       * If the expression is changed by any level of modifier, it should be
+       * rerun from the start (it may lead to infinite loop though.)
+       * 1. If the expression is constant, evaluate it right away.
+       *    - a + 5 * 2 -> a + 10
+       * 2. Expand all the columns using distributive property - this will allow
+       *    columns to appear at the root level.
+       *    - (a + 3) * 5 = 3 -> a * 5 + 3 * 5 = 3
+       * 3. If the same column appears multiple times, it can be reduced to 
+       *    appear only once, effectively becoming SARG.
+       *    This should only apply in append -> multiply order, the other
+       *    doesn't apply at all.
+       *    - a * 2 - a * 1 = 0 -> a = 0
+       *    - a + 3 - a + 5 = 0 -> 8 = 0 -> FALSE
+       *    - (a + 2) * (a + 3) -> This shouldn't be applied at all.
+       * 4. If the expression is identity function, convert them to raw entry.
+       *    - a * 1 + (a - 0) = 0 -> a + a = 0
+       * 5. Push all constants, or any other insignificant columns to the right.
+       *    This will require inverting the direction of expression.
+       *    'Insignificant' column can be decided randomly.
+       *    - a + 5 = 0 -> a = -5.
+       */
       // TODO Write stuff...
     }
     return { expr, state };
