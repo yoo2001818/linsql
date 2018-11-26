a034c81c1560674cbccb0d746a3fce2c876e7e60
Duknam Yoo <yoo2001818@gmail.com>
2018-11-26 23:59:36 +0900

    Add table types

--- src/table/index.ts
+++ src/table/index.ts
@@ -1,5 +1,26 @@
-export default class Table {
-  getPlan() {
+export interface Index {
+  name: string,
+  order: [string, boolean][],
+  unique: boolean,
+  cardinality: number,
+  count: number,
+}
 
-  }
+export interface BaseTable {
+  name: string,
+  columns: string[],
+}
+
+export interface NormalTable extends BaseTable {
+  indexes: Index[],
+  order: [string, boolean][],
+  count: number,
+  fetch: (indexName?: string, low?: any[], high?: any[],
+    lte?: boolean, gte?: boolean) => Iterator<Promise<any[]>>,
+}
+
+export interface ArrayTable extends BaseTable {
+  order: [string, boolean][],
+  count: number,
+  fetch: (low?: any[], high?: any[]) => Iterator<Promise<any[]>>,
 }
