554004278a33a1a935a44440d25fc1c95462ed1f
Duknam Yoo <yoo2001818@gmail.com>
2019-04-17 23:59:16 +0900

    Add findSargsRangeIndex

--- src/planner/getSargsRange.ts
+++ src/planner/getSargsRange.ts
@@ -1,6 +1,7 @@
+import { Expression } from 'yasqlp';
 import createRangeSetModule, { RangeSet } from 'range-set';
 
-import { NormalTable } from '../table';
+import { NormalTable, Index } from '../table';
 
 const numberSet = createRangeSetModule({
   compare: (a: number, b: number) => a - b,
@@ -25,11 +26,47 @@ const stringSet = createRangeSetModule({
   negativeInfinity: stringNegativeInfinity,
 });
 
+interface RangeResult {
+  name: string,
+  set: RangeSet<any>,
+}
+
+export default function findSargsRangeIndex(
+  table: NormalTable, index: Index, where: Expression,
+): RangeResult {
+  // We have to handle more than one columns - therefore, we have to check
+  // toset can be fulfilled. If we can't, the leftmost column can be used
+  // anyway...
+  //
+  // Right columns can't be used until left columns are specified using '=',
+  // and only rightmost column can use range queries like '>', '<'.
+  // Note that we don't have to use all the columns - we still can use
+  // range queries when the columns are not used completely.
+  function traverseStep(set: RangeSet<any>, expr: Expression): RangeSet<any> {
+    switch (expr.type) {
+      case 'logical':
+        if (expr.op === '&&') {
+          return expr.values.reduce(
+            (prev, child) => traverseStep(prev, child),
+            set);
+        } else if (expr.op === '||') {
+          // Run two indices and check if they're mergeable - 
+        }
+        break;
+      case 'binary':
+        if (expr.left.type === 'column' && expr.left.table === table.name) {
+        }
+        break;
+    }
+  }
+  traverseStep([], where);
+}
+
 export default function findSargsRange(
   table: NormalTable, where: Expression,
 ) {
-  const sets: { [key: string]: RangeSet<any> } = {};
+  const sets: RangeResult[] = [];
   for (let index of table.indexes) {
-
+    findSargsRangeIndex(table, index, where);
   }
 }
