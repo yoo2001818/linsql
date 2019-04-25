888471366e0b576e1af7f125b62a5dabe381b1cd
Duknam Yoo <yoo2001818@gmail.com>
2019-04-25 23:59:18 +0900

    Add tree structure comment

--- src/planner/getSargsRange.ts
+++ src/planner/getSargsRange.ts
@@ -31,57 +31,75 @@ interface RangeResult {
   set: RangeSet<any>,
 }
 
+// We have to handle more than one columns, to be exact, N 'equal' columns
+// and one range columns.
+//
+// Right columns can't be used until left columns are specified using '=',
+// and only rightmost column can use range queries like '>', '<'.
+// Note that we don't have to use all the columns - we still can use
+// range queries when the columns are not used completely.
+//
+// However, there is one exception - a expression can be converted to one
+// range scan.
+// a > 3 OR (a = 3 AND b > 5)
+// a >= 3 AND (a > 3 OR b > 5)
+// ...both can be converted into ((3, 5) ... inf), for (a, b) index.
+// 
+// First one is pretty straightforward - since both ranges are inside (a, b)
+// index's range, they just have to merged together.
+// Second one can be a little tricky to derive the ranges. a >= 3 is easy
+// to derive, however, (a > 3 OR b > 5) requires the prior knowledge of
+// a >= 3. 
+// 
+// Since second one is too hard to process, let's just process the first one.
+//
+// Expressions merged using 'AND' is simple to process - we can just ignore
+// irrelevant columns. Of course, all columns must be present, but still it's
+// simple enough.
+// On the other hand, 'OR' requires all predicates are relevant to the index.
+// 
+// We need to note that range queries can only be done at the rightmost
+// position, this is absolutely important.
+// Other than non-range queries, we can consider them as a group, since
+// their order shouldn't matter before picking an index.
+//
+// For example, (a = 1 AND b = 1 AND c = 1) AND d > 5
+// d > 5 should be rightmost one, but others are not.
+//
+// (a = 1 OR b = 1 OR c = 1) is resolved using index merge, but this is out
+// of scope for now.
+//
+// For this reason, ranges and non-ranges can be separated.
+// However, this is not enough - we have to resolve
+// a > 3 OR (a = 3 AND b > 5).
+//
+// This can be implemented by putting expression inside equal predicate.
+// Then it can be freely converted into ranges, no matter how the indices are
+// constructed.
+// 
+// Therefore, we'll construct a tree-like structure using this.... per column.
+//
+// (a = 1 AND b = 1 AND c = 1) OR a > 3
+// a > 3 OR (a = 3 AND b > 5)
+//     
+//      root
+//       |
+//       a
+//     /   \
+// a > 3  a = 3
+//          |
+//          b
+//          |
+//        b > 5
+
+interface RangeNode {
+
+}
+  
 export default function findSargsRange(
   name: string, table: NormalTable, where: Expression,
 ) {
   const sets: RangeResult[] = [];
-
-  // We have to handle more than one columns, to be exact, N 'equal' columns
-  // and one range columns.
-  //
-  // Right columns can't be used until left columns are specified using '=',
-  // and only rightmost column can use range queries like '>', '<'.
-  // Note that we don't have to use all the columns - we still can use
-  // range queries when the columns are not used completely.
-  //
-  // However, there is one exception - a expression can be converted to one
-  // range scan.
-  // a > 3 OR (a = 3 AND b > 5)
-  // a >= 3 AND (a > 3 OR b > 5)
-  // ...both can be converted into ((3, 5) ... inf), for (a, b) index.
-  // 
-  // First one is pretty straightforward - since both ranges are inside (a, b)
-  // index's range, they just have to merged together.
-  // Second one can be a little tricky to derive the ranges. a >= 3 is easy
-  // to derive, however, (a > 3 OR b > 5) requires the prior knowledge of
-  // a >= 3. 
-  // 
-  // Since second one is too hard to process, let's just process the first one.
-  //
-  // Expressions merged using 'AND' is simple to process - we can just ignore
-  // irrelevant columns. Of course, all columns must be present, but still it's
-  // simple enough.
-  // On the other hand, 'OR' requires all predicates are relevant to the index.
-  // 
-  // We need to note that range queries can only be done at the rightmost
-  // position, this is absolutely important.
-  // Other than non-range queries, we can consider them as a group, since
-  // their order shouldn't matter before picking an index.
-  //
-  // For example, (a = 1 AND b = 1 AND c = 1) AND d > 5
-  // d > 5 should be rightmost one, but others are not.
-  //
-  // (a = 1 OR b = 1 OR c = 1) is resolved using index merge, but this is out
-  // of scope for now.
-  //
-  // For this reason, ranges and non-ranges can be separated.
-  // However, this is not enough - we have to resolve
-  // a > 3 OR (a = 3 AND b > 5).
-  //
-  // This can be implemented by putting expression inside equal predicate.
-  // Then it can be freely converted into ranges, no matter how the indices are
-  // constructed.
-  
   function traverseStep(
     index: Index,
     expr: Expression,
